01a5c6beb2340feaa921987af0c73910b06363d1
Divyansh Raj <divyansh.raj@tanglesolutions.com>
2024-08-28 13:31:37 +0530

    auth

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -14,9 +14,6 @@ import { Router } from '@angular/router';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  private isRefreshing = false;
-  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-
   constructor(private loginService: LoginService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,11 +25,12 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401 && error.error.message === "JWT token is expired") {
-          return this.handle401Error(request, next);
-        } else {
-          return throwError(error);
+        if (error.status === 401) {
+       
+          this.loginService.logout();
+          this.router.navigate(['/login']); 
         }
+        return throwError(error);
       })
     );
   }
@@ -40,38 +38,8 @@ export class AuthInterceptor implements HttpInterceptor {
   private addToken(request: HttpRequest<any>, token: string) {
     return request.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}` 
       }
     });
-  }
-
-  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
-    if (!this.isRefreshing) {
-      this.isRefreshing = true;
-      this.refreshTokenSubject.next(null);
-
-      return this.loginService.refreshToken().pipe(
-        switchMap((token: any) => {
-          this.isRefreshing = false;
-          this.refreshTokenSubject.next(token.accessToken);
-          this.loginService.saveToken(token.accessToken, token.refreshToken);
-          return next.handle(this.addToken(request, token.accessToken));
-        }),
-        catchError((error) => {
-          this.isRefreshing = false;
-          this.loginService.logout();
-          this.router.navigate(['/login']);
-          return throwError(error);
-        })
-      );
-    } else {
-      return this.refreshTokenSubject.pipe(
-        filter(token => token != null),
-        take(1),
-        switchMap(jwt => {
-          return next.handle(this.addToken(request, jwt));
-        })
-      );
-    }
   }
 }
